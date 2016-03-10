bf232e4c37c9582272abf9cb3eacaf1fd194f406
Cezar Vinícius de O. Pretto <cezar@planosassessoria.com.br>
2016-03-10 10:40:40 -0300

    Add buildcontrol task to Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,7 +16,8 @@ module.exports = function (grunt) {
   require('jit-grunt')(grunt, {
     useminPrepare: 'grunt-usemin',
     ngtemplates: 'grunt-angular-templates',
-    cdnify: 'grunt-google-cdn'
+    cdnify: 'grunt-google-cdn',
+    buildcontrol: 'grunt-build-control'
   });
 
   // Configurable paths for the application
@@ -220,7 +221,7 @@ module.exports = function (grunt) {
             }
           }
       }
-    }, 
+    },
 
     // Renames files for browser caching purposes
     filerev: {
@@ -422,6 +423,27 @@ module.exports = function (grunt) {
       unit: {
         configFile: 'test/karma.conf.js',
         singleRun: true
+      }
+    },
+
+    buildcontrol: {
+      options: {
+        dir: 'dist',
+        commit: true,
+        push: true,
+        message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+      },
+      pages: {
+        options: {
+          remote: 'https://github.com/cezarpretto/dm-inscricoes.git',
+          branch: 'gh-pages'
+        }
+      },
+      local: {
+        options: {
+          remote: '../',
+          branch: 'build'
+        }
       }
     }
   });
--- app/scripts/services/connectionconfig.js
+++ app/scripts/services/connectionconfig.js
@@ -9,7 +9,8 @@
  */
 angular.module('dmInscricoesApp')
   .constant('connectionConfig', {
-    "ip": "http://localhost:3000/v1/"
+    "ip": "https://protected-ocean-80696.herokuapp.com/v1/"
   });
 
   //Heroku IP https://protected-ocean-80696.herokuapp.com/v1/
+  //Local http://localhost:3000/v1/
